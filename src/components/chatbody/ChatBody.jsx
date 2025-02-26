7f7bf0e0c39f498a600ac2cc771f4f3ac98c2685
MoiseFlori <moiseflori@outlook.com>
2025-02-26 19:00:42 +0200

    mobile optimization

--- src/components/chatbody/ChatBody.jsx
+++ src/components/chatbody/ChatBody.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './ChatBody.module.css';
 import Message from '../message/Message';
+import { useRef, useEffect } from 'react';
 
 const ChatBody = ({ messages }) => {
+  const messagesEndRef = useRef(null);
 
-    if (!Array.isArray(messages)) {
-      console.error('Eroare: `messages` nu este un array.', messages);
-      return <div className={styles.chatBody}>No messages yet.</div>;
-    }
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
+  if (!Array.isArray(messages)) {
+    console.error('Eroare: `messages` nu este un array.', messages);
+    return <div className={styles.chatBody}>No messages yet.</div>;
+  }
   return (
     <div className={styles.chatBody}>
       {messages.map((msg, index) => (
@@ -15,6 +21,7 @@ const ChatBody = ({ messages }) => {
           <Message message={msg.text} sender={msg.sender} image={msg.image} />
         </div>
       ))}
+      <div ref={messagesEndRef} /> 
     </div>
   );
 };
